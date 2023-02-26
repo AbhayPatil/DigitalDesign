e4f387594757eebfbd5b117e4a4d7782479ad602
Abhay <abhay9874patil@gmail.com>
2023-02-26 12:57:09 -0800

    initial

---
+++ SystemVerilog/Syntax/task_and_function.sv
@@ -0,0 +1,34 @@
+
+module task_and_function ();
+
+  logic [3:0] num;
+
+  // Task
+  // 1. Did not compile without explicit input/output
+  // declaration in iverilog.
+  task IncT;
+    output [3:0] inc;
+    input [3:0] num;
+    #2;
+    inc = num+1;
+  endtask
+
+  // Function
+  function logic[3:0] IncF (logic [3:0] num);
+    IncF = num+1;
+  endfunction
+
+  initial begin
+    num = 4'd0;
+    repeat(16) begin
+      $display("Num %d\n", num);
+      
+      // Call function
+      // num = IncF(num);
+
+      // Call a task
+      IncT(num, num);
+    end
+  end
+
+endmodule
